3f2a04406591f77dfbc1ba955d1dc2153c24dbfe
Daliak <dalia.kanan@gmail.com>
2020-12-01 06:57:42 -0800

    fix comment

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -5,6 +5,6 @@ xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch " &
 sleep 5
 xterm  -e  " rosrun rviz rviz -c/rvizConfig/view_navigation.rviz " &
 sleep 5
-xterm  -e  " roslaunch rosrun add_markers add_markers " &
+xterm  -e  " rosrun add_markers add_markers " &
 sleep 5
-xterm  -e  " roslaunch rosrun pick_objects pick_objects "
+xterm  -e  " rosrun pick_objects pick_objects "
